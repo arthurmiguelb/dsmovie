fed523a15feaaa26d61efb947a5192c95d23b7c2
Arthur Miguel <arthurmiguelb1@gmail.com>
2022-03-13 20:23:52 -0300

    First request

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,8 +5,8 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1> DSmovie </h1>
-                    <a href="https://github.com/arthurmiguelb">
+                    <h1>DSmovie</h1>
+                    <a href='https://github.com/arthurmiguelb'>
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/arthurmiguelb</p>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,16 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 //import MOVIE from "components/MovieScore";
 import Pagination from "components/Navbar/Pagination";
+import { BASE_URL } from "ultils/requests";
 
 function Listing() {
+
+    // forma errada
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        })
 
     return (
         <>
