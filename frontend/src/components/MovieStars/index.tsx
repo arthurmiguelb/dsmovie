c88444039b90299dabd833dfa488677c1272941b
Arthur Miguel <arthurmiguelb1@gmail.com>
2022-03-13 23:31:30 -0300

    commit netfly

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -32,10 +32,10 @@ function getFills(score: number) {
 }
 
 function Star({ fill }: StarProps) {
-    if (fill == 0) {
+    if (fill === 0) {
         return <StarEmpty />
     }
-    else if (fill == 1) {
+    else if (fill === 1) {
         return <StarFull />
     }
     else {
