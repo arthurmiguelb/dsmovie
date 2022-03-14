88cb64f558684dcfeb53899091ee0487465a7572
Arthur Miguel <arthurmiguelb1@gmail.com>
2022-03-13 21:07:32 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,18 +2,33 @@ import axios from "axios";
 import MovieCard from "components/MovieCard";
 //import MOVIE from "components/MovieScore";
 import Pagination from "components/Navbar/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "ultils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
+
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+
+    }, []);
+
     // forma errada
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data);
-        })
+
 
     return (
         <>
+            <p>{pageNumber}</p>
+
             <Pagination />
             <div className="container">
                 <div className="row">
@@ -40,15 +55,15 @@ function Listing() {
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
-                    </div>                  
+                    </div>
                 </div>
 
 
-             </div>   
-            
+            </div>
+
         </>
     );
-                        
+
 }
 
 
